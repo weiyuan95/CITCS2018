9a71f877999fd9b82dea643ed0412e2558c11991
weiyuan95 <35588002+weiyuan95@users.noreply.github.com>
2018-09-21 12:55:12 +0800

    added in more testing for get and post requests

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,7 +13,9 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
-    url(r'^test', hello.views.test, name='test'),
-    url(r"^test2", hello.views.test2, name="test2"),
+    url(r'^test$', hello.views.test, name='test'),
+    url(r"^test2$", hello.views.test2, name="test2"),
+    url(r"^testing_get", hello.views.testing_get, name="testing"),
+    url(r"^testing_post", hello.views.testing_post, name="testing"),
     path('admin/', admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -24,12 +24,27 @@ def db(request):
 
 
 # TEST CODE BELOW:
-@csrf_exempt
+# @csrf_exempt
 def test(request):
     return HttpResponse("Waow")
 
 
-@csrf_exempt
+# @csrf_exempt
 def test2(request):
     return HttpResponse(json.dumps({"lol": "wow"}))
 
+
+def testing_get(request):
+    if request.method == "GET":
+        # example of getting the key "hello" from a GET request
+        print(request.GET["hello"])
+        return HttpResponse(request.GET)
+
+
+# need to wrap method with csrf_exempt for post requests
+@csrf_exempt
+def testing_post(request):
+    if request.method == "POST":
+        return HttpResponse(request.POST)
+
+    return HttpResponse("No post request received.")
