5f2c289fd7c358cf2dcb0a0be2def1d427a78413
weiyuan95 <35588002+weiyuan95@users.noreply.github.com>
2018-09-21 22:00:16 +0800

    refactored hello folder to main

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,14 +4,14 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import main.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r"^square$", hello.views.squares, name="squares"),
+    url(r'^$', main.views.index, name='index'),
+    url(r"^square$", main.views.squares, name="squares"),
     path('admin/', admin.site.urls),
 ]
