8a27ff826179a2f81be259c9528b8d170529a67b
weiyuan95 <35588002+weiyuan95@users.noreply.github.com>
2018-09-21 17:46:20 +0800

    remove unneeded code from views

--- hello/views.py
+++ hello/views.py
@@ -4,23 +4,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
-from .models import Greeting
-
-
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
 
 
 # TEST CODE BELOW:
