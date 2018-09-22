b05f6048c812a73f6c32ccad4f9fe03977dee802
weiyuan95 <35588002+weiyuan95@users.noreply.github.com>
2018-09-22 10:56:14 +0800

    updated prime sum solution

--- main/wy/solutions.py
+++ main/wy/solutions.py
@@ -1,28 +1,54 @@
+import math
+
 def get_square(num):
     return num ** 2
 
 
 def prime_sum(num):
-    result = []
+    temp = []
+    used_biggest = []
+    not_found = True
 
-    while num > 0:
-        for i in range(num, -1, -1):
-            if is_prime(i) and is_prime(num - i) is not False:
-                result.append(i)
-                num -= i
-                break
-    return result
+    while not_found:
+        temp = []
+        temp_num = num
+
+        while temp_num > 0:
+
+            for i in range(temp_num, -1, -1):
+                if is_prime(i) and i not in used_biggest:
+                    temp.append(i)
+                    temp_num -= i
+                    break
+
+            if temp_num == 1:
+                temp.append(1)
+                temp_num = 0
+
+        flag = [not is_prime(j) for j in temp]
+
+        if not any(flag):
+            not_found = False
+        else:
+            used_biggest.append(temp[0])
+
+    return temp
 
 
 def is_prime(number):
-    if number == 1:
+    if number == 2:
+        return True
+    if number % 2 == 0 or number <= 1:
         return False
 
-    for i in range(2, number + 1):
-        if i != number and number % i == 0:
+    sqr = int(math.sqrt(number)) + 1
+
+    for i in range(3, sqr, 2):
+        if number % i == 0 and number != i:
             return False
 
     return True
 
 
-print(prime_sum(2))
+if __name__ == "__main__":
+    print(prime_sum(4))
