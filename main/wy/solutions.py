e9c2ab5563c7c3f024ef4318859e4a9429630d3a
weiyuan95 <35588002+weiyuan95@users.noreply.github.com>
2018-09-22 15:02:16 +0800

    fixed decimal + float bug

--- main/wy/solutions.py
+++ main/wy/solutions.py
@@ -75,7 +75,7 @@ def calculate_expenses(people, expenses):
 
         amount_payable_to_each = Decimal(amount / (total_people - len(excluded_people))).quantize(0, rounding=ROUND_HALF_UP)
 
-        amt_payable = round(amount_payable_to_each, 2)
+        amt_payable = amount_payable_to_each, 2
 
         if to_pay_person in excluded_people:
             expense_dict[to_pay_person] += amount
